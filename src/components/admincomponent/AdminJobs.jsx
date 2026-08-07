348679a7ab1aa64b6f6ca016307472a036a241bf
kaioumdev <afrankaiyum34@gmail.com>
2026-08-07 13:14:22 +0600

    fix the errors and solve the issue and import file successfully to solve the errors

--- src/components/admincomponent/AdminJobs.jsx
+++ src/components/admincomponent/AdminJobs.jsx
@@ -5,7 +5,7 @@ import { Button } from "../ui/button";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import AdminJobsTable from "./AdminJobsTable";
-import useGetAllAdminJobs from "@/hooks/useGetAllJAdminobs";
+import useGetAllAdminJobs from "@/hooks/useGetAllAdminJobs";
 import { setSearchJobByText } from "@/redux/jobSlice";
 import { Plus, Search } from "lucide-react";
 
